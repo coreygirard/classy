085098758837129b450ce313e130da2fffb25a98
Corey Girard <corey.r.girard@gmail.com>
2017-12-06 15:02:28 -0500

    Fixing code coverage

--- tests.py
+++ tests.py
@@ -19,7 +19,58 @@ class TestClassy(unittest.TestCase):
         c = classy.Classifier(data)
 
         result = c.classify('a')
-        #self.assertEqual(result['Class A'],max(result.values()))
+        self.assertEqual(result['Class A'],max(result.values()))
+
+    def test_custom_parse(self):
+        data = {'1':['a,b,c',
+                     'd,e f'],
+                '2':['i,j,k',
+                     'x y,z']}
+
+        c = classy.Classifier(data)
+
+        self.assertEqual(c.prior,{'1': 2, '2': 2})
+        self.assertEqual(c.getPrior(),{'1': 0.5, '2': 0.5})
+        self.assertEqual(c.word,{'1': {'abc': 1,
+                                       'de': 1,
+                                       'f': 1},
+                                 '2': {'ijk': 1,
+                                       'x': 1,
+                                       'yz': 1}})
+        self.assertEqual(c.corpus,{'abc': 1,
+                                   'de': 1,
+                                   'f': 1,
+                                   'ijk': 1,
+                                   'x': 1,
+                                   'yz': 1})
+
+        def newParse(t):
+            return [i for i in t.split(',') if i != '']
+
+        c2 = classy.Classifier(data,f=newParse)
+
+        self.assertEqual(c2.prior,{'1': 2, '2': 2})
+        self.assertEqual(c2.getPrior(),{'1': 0.5, '2': 0.5})
+        self.assertEqual(c2.word,{'1': {'a': 1,
+                                        'b': 1,
+                                        'c': 1,
+                                        'd': 1,
+                                        'e f': 1},
+                                  '2': {'i': 1,
+                                        'j': 1,
+                                        'k': 1,
+                                        'x y': 1,
+                                        'z': 1}})
+        self.assertEqual(c2.corpus,{'a': 1,
+                                    'b': 1,
+                                    'c': 1,
+                                    'd': 1,
+                                    'e f': 1,
+                                    'i': 1,
+                                    'j': 1,
+                                    'k': 1,
+                                    'x y': 1,
+                                    'z': 1})
 
 
 
