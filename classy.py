d91fd623417d6e37746b916c6ab4dcafa22b050e
Corey Girard <corey.r.girard@gmail.com>
2018-09-01 14:15:39 -0400

    Linting and testing

--- classy.py
+++ classy.py
@@ -1,15 +1,16 @@
 import re
 
+
 class Classifier(object):
-    def __init__(self,i,f=None,threshold=None):
+    def __init__(self, i, f=None, threshold=None):
         '''
-        >>> c = Classifier({'1':['a b',
-        ...                      'a c'],
-        ...                 '2':['d f',
-        ...                      'e f']})
+        >>> c = Classifier({'1': ['a b',
+        ...                       'a c'],
+        ...                 '2': ['d f',
+        ...                       'e f']})
         >>> c.prior == {'1': 2, '2': 2}
         True
-        >>> c.getPrior() == {'1': 0.5, '2': 0.5}
+        >>> c.get_prior() == {'1': 0.5, '2': 0.5}
         True
         >>> c.word == {'1': {'a': 2,
         ...                  'b': 1,
@@ -36,112 +37,124 @@ class Classifier(object):
 
         self.add(i)
 
-    def add(self,i):
-        for k,v in i.items():
+    def add(self, i):
+        for k, v in i.items():
             for e in v:
-                self.addLine(k,e)
+                self.add_line(k, e)
 
         self.total = sum(self.prior.values())
 
-    def addLine(self,k,e):
+    def add_line(self, k, e):
         '''
         >>> c = Classifier({})
+
         >>> c.prior == {}
         True
+
         >>> c.word == {}
         True
+
         >>> c.corpus == {}
         True
-        >>> c.addLine('1','a b b')
-        >>> c.prior == {'1':1}
+
+        >>> c.add_line('1', 'a b b')
+        >>> c.prior == {'1': 1}
         True
-        >>> c.word == {'1':{'a': 1,
-        ...                 'b': 2}}
+
+        >>> c.word == {'1': {'a': 1,
+        ...                  'b': 2}}
         True
+
         >>> c.corpus == {'a': 1,
         ...              'b': 2}
         True
         '''
 
-        self.prior[k] = self.prior.get(k,0)+1
+        self.prior[k] = self.prior.get(k, 0)+1
         if k not in self.word.keys():
             self.word[k] = {}
 
         for i in self.parse(e):
-            self.word[k][i] = self.word[k].get(i,0)+1
-            self.corpus[i] = self.corpus.get(i,0)+1
+            self.word[k][i] = self.word[k].get(i, 0)+1
+            self.corpus[i] = self.corpus.get(i, 0)+1
 
-    def parse(self,text):
+    def parse(self, text):  #pylint: disable=method-hidden
         '''
         >>> Classifier({}).parse('a b c')
         ['a', 'b', 'c']
         '''
 
         text = text.lower()
-        text = re.sub(r'[^a-z0-9 ]',r'',text)
+        text = re.sub(r'[^a-z0-9 ]', r'', text)
         return [i for i in text.split(' ') if i != '']
 
-    def getPrior(self):
-        return {k:v/self.total for k,v in self.prior.items()}
+    def get_prior(self):
+        return {k: v/self.total for k, v in self.prior.items()}
 
-    def classifyWord(self,w):
+    def classify_word(self, w):
         '''
-        >>> c = Classifier({'1':['a b',
+        >>> c = Classifier({'1': ['a b',
+        ...                       'a c'],
+        ...                 '2': ['d f',
+        ...                       'e f']})
+        >>> c.classify_word('a') == {'1': 0.75, '2': 0.25}
+        True
+        >>> c.classify_word('b') == {'1': 2/3,  '2': 1/3}
+        True
+        >>> c.classify_word('f') == {'1': 0.25, '2': 0.75}
+        True
+        '''
+
+        temp = {k: (v.get(w, 0)+1) for k, v in self.word.items()}
+        return {k: v/sum(temp.values()) for k, v in temp.items()}
+
+    def classify(self, i):
+        '''
+        >>> c = Classifier({'1': ['a b',
         ...                      'a c'],
-        ...                 '2':['d f',
-        ...                      'e f']})
-        >>> c.classifyWord('a') == {'1': 0.75, '2': 0.25}
-        True
-        >>> c.classifyWord('b') == {'1': 2/3,  '2': 1/3}
-        True
-        >>> c.classifyWord('f') == {'1': 0.25, '2': 0.75}
-        True
-        '''
-
-        temp = {k:(v.get(w,0)+1) for k,v in self.word.items()}
-        return {k:v/sum(temp.values()) for k,v in temp.items()}
-
-    def classify(self,i):
-        '''
-        >>> c = Classifier({'1':['a b',
-        ...                      'a c'],
-        ...                 '2':['d f',
+        ...                 '2': ['d f',
         ...                      'e f']})
         >>> temp = c.classify('a f')
         >>> temp['1'] == temp['2']
         True
+
         >>> r = c.classify('a b')
         >>> r['1'] == max(r.values())
         True
+
         >>> r = c.classify('a d')
         >>> r['1'] == max(r.values())
         True
+
         >>> r = c.classify('b f')
         >>> r['2'] == max(r.values())
         True
+
         >>> c.threshold = 0.8
         >>> c.classify('a a a') == None
         True
+
         >>> c.classify('x y z') == None
         True
         '''
 
         i = self.parse(i)
 
-        p = self.getPrior()
+        p = self.get_prior()
 
         for word in i:
-            c = self.classifyWord(word)
-            for k,v in c.items():
+            c = self.classify_word(word)
+            for k, v in c.items():
                 p[k] *= v
 
         total = sum(p.values())+1
-        p = {k:v/total for k,v in p.items()}
+        p = {k: v/total for k, v in p.items()}
 
-        if self.threshold == None:
+        if self.threshold is None:
             return p
-        else:
-            for k,v in p.items():
-                if v >= self.threshold:
-                    return k
-            return None
+
+        for k, v in p.items():
+            if v >= self.threshold:
+                return k
+
+        return None
---
+++ test_classy.py
@@ -0,0 +1,71 @@
+import classy
+
+
+
+def test_basic_classify():
+    data = {'Class A': ['a b c',
+                        'a c e',
+                        'b a c',
+                        'e d f'],
+            'Class B': ['e f g',
+                        'e y v',
+                        'f e w',
+                        'f e v',
+                        'v f d']}
+
+    c = classy.Classifier(data)
+
+    result = c.classify('a')
+    assert result['Class A'] == max(result.values())
+
+
+def test_custom_parse():
+    data = {'1': ['a,b,c',
+                  'd,e f'],
+            '2': ['i,j,k',
+                  'x y,z']}
+
+    c = classy.Classifier(data)
+
+    assert c.prior == {'1': 2, '2': 2}
+    assert c.get_prior() == {'1': 0.5, '2': 0.5}
+    assert c.word == {'1': {'abc': 1,
+                            'de': 1,
+                            'f': 1},
+                      '2': {'ijk': 1,
+                            'x': 1,
+                            'yz': 1}}
+    assert c.corpus == {'abc': 1,
+                        'de': 1,
+                        'f': 1,
+                        'ijk': 1,
+                        'x': 1,
+                        'yz': 1}
+
+    def new_parse(t):
+        return [i for i in t.split(',') if i != '']
+
+    c2 = classy.Classifier(data, f=new_parse)
+
+    assert c2.prior == {'1': 2, '2': 2}
+    assert c2.get_prior() == {'1': 0.5, '2': 0.5}
+    assert c2.word == {'1': {'a': 1,
+                             'b': 1,
+                             'c': 1,
+                             'd': 1,
+                             'e f': 1},
+                       '2': {'i': 1,
+                             'j': 1,
+                             'k': 1,
+                             'x y': 1,
+                             'z': 1}}
+    assert c2.corpus == {'a': 1,
+                         'b': 1,
+                         'c': 1,
+                         'd': 1,
+                         'e f': 1,
+                         'i': 1,
+                         'j': 1,
+                         'k': 1,
+                         'x y': 1,
+                         'z': 1}
